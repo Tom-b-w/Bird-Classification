eb126024cb409b52ce5eb1afcd485a0701c5c935
Tom-b-w <tbw31415@126.com>
2025-09-21 17:18:22 +0800

    Update config.py

--- Bird-Sound-Recogniser-main/config.py
+++ Bird-Sound-Recogniser-main/config.py
@@ -1,4 +1,4 @@
 SMTP_SERVER = 'smtp.qq.com'
 SMTP_PORT =465
-EMAIL_SENDER = '2437476553@qq.com'
-EMAIL_PASSWORD = 'dtaapibzigrzeada'  # 注意：是授权码不是登录密码！
+EMAIL_SENDER = ''
+EMAIL_PASSWORD = '*********'  # 注意：是授权码不是登录密码！
